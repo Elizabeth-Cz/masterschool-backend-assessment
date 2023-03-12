267ff80e3225871db54a8fc12977afda2a8b6a91
Elizabeth Czarny <liz.cz91@gmail.com>
2023-03-12 18:48:46 +0100

    Part VII - Error Handling Middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const photoRoutes = require('./routes/photoRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -12,9 +13,12 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
 app.use('/api/photos', photoRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/favorites', favoritesRoutes);
+
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Unsplash API!' });
